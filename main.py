79ccde4bf598a7f68ce4a393878e8b80aa23fc19
LUKERHYS <rhystechnology@gmail.com>
2019-02-23 13:43:19 +0000

    Now with an additional level of directories that is for each day of th month. This is basic DAY_1 - 31 loop how ever this will eventually have the day date formatted correctly and also the correct number of days for that month rather than a generic 31 days

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-#Import Modules Here
+# Import Modules Here
 import os
 import datetime
 import calendar
 
-#My Functions
-##Pulling in current Month as a number
-now = datetime.date.today().year
-month = datetime.datetime.now().strftime("%m")
-monthDigit = month.strip("0")
+
+# Cleansing of date feilds to generate
+# a single digit to represent 
+# the current month 
+now = datetime.date.today().year                # Reutrns the full date time
+month = datetime.datetime.now().strftime("%m")  # Strips the above down to current month as int. i.e. February is 02 
+monthDigit = month.strip("0")                   # Strips the 0 from the above 2 digit month reference
 
 
-#Set Parent Location
+# Set Parent Location
 os.chdir(input("Where would you like to create the directories?: "))
 
 
-#Create Month Directory + Card Sub-Directories + Batch Sub-Directories
+# Nested For loops to generate directory structure and naming 
 cardNums = ['Card_1', 'Card_2', 'Card_3']
 for cardNum in cardNums:
-    for f in range(1, 21, 1):
-        batchNum = "Batch" + "_" + str(f)
-        os.makedirs(os.path.join(calendar.month_name[int(monthDigit)] + "_" + str(now), cardNum, batchNum))
-
-       
-#/Users/lukerhys/Desktop/Test
-
-#int(output), cardNum, batchNum
+        for f in range(1, 21, 1):
+                batchNum = "Batch" + "_" + str(f)
+                for d in range(1, 32, 1):
+                        day = "Day" + "_" + str(d)
+                        os.makedirs(os.path.join(calendar.month_name[int(monthDigit)] + "_" + str(now), str(day), cardNum, batchNum))
